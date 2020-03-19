9cbcd4883c93fb9eac9c6e828b388f5e98b010b1
WilliamJGrace <williamjgrace@outlook.com>
2020-03-19 15:28:58 +0000

    user can choose city to display weather

--- src/interface.js
+++ src/interface.js
@@ -5,6 +5,26 @@ $(document).ready(function() {
     $('#current-temperature').attr('class', thermostat.getCurrentUsage());
   };
 
+  function displayWeather(city) {
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+    var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
+    var units = '&units=metric';
+    $.get(url + token + units, function(data) {
+      $('#outside-temperature').text(data.main.temp);
+    });
+
+  };
+
+
+
+  // $('#outside-temperature').change(function(){
+  displayWeather("London");
+
+  $('#current-city').change(function() {
+  var city = $('#current-city').val();
+  displayWeather(city);
+})
+
 
 
 
